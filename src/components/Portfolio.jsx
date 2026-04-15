4179c1580a1f111edfd4f5db4d771a549341950b
Pratyush Ranjan <144735922+Pratyush271299@users.noreply.github.com>
2026-04-15 14:35:20 +0530

    Refactor portfolio items and update IDs

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import athenaChat from '../assets/portfolio/athenaChat.png';
 import vidTube from '../assets/portfolio/vidTube.png';
-import chamaksaathi2 from '../assets/portfolio/chamaksaathi2.png';
 import shopper from '../assets/portfolio/shopper.png';
 import dice from '../assets/portfolio/dice.png';
 
@@ -9,30 +8,24 @@ const Portfolio = () => {
   const portfolios = [
     {
       id: 1,
-      src: chamaksaathi2,
-      demo: 'https://www.chamaksaathi.com',
-      github: 'https://github.com/Pratyush271299/'
-    },
-    {
-      id: 2,
       src: vidTube,
       demo: 'https://youtubeclonevidtube.netlify.app/',
       github: 'https://github.com/Pratyush271299/VidTube'
     },
     {
-      id: 3,
+      id: 2,
       src: athenaChat,
       demo: 'https://athenachat.netlify.app/',
       github: 'https://github.com/Pratyush271299/Athena-AI-Chatbox'
     },
     {
-      id: 4,
+      id: 3,
       src: shopper,  
       demo: 'https://endearing-griffin-8e1435.netlify.app/',
       github: 'https://github.com/Pratyush271299/ECommerce-Website'
     },
     {
-      id: 5,
+      id: 4,
       src: dice, 
       demo: 'https://dice-numbers-game.netlify.app/',
       github: 'https://github.com/Pratyush271299/Dice-Game'
@@ -54,11 +47,14 @@ const Portfolio = () => {
 
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
           {portfolios.map(({ id, src, demo, github }) => (
-            <div key={id} className="shadow-md shadow-gray-600 rounded-lg duration-200 hover:scale-105">
+            <div
+              key={id}
+              className="shadow-md shadow-gray-600 rounded-lg duration-200 hover:scale-105"
+            >
               <img
                 src={src}
-                alt=""
-                className="rounded-md "
+                alt="project"
+                className="rounded-md"
               />
               <div className="flex items-center justify-center">
                 <a href={demo} target="_blank" rel="noopener noreferrer">
@@ -67,7 +63,7 @@ const Portfolio = () => {
                   </button>
                 </a>
                 <a href={github} target="_blank" rel="noopener noreferrer">
-                  <button className="px-6 py-3 w-1/2 m-4 duration-200 hover:scale-105">
+                  <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
                     Code
                   </button>
                 </a>
