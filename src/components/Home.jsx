7994a6b51af0d6d5d3464c792969aee195a4e741
Pratyush Ranjan <144735922+Pratyush271299@users.noreply.github.com>
2026-04-15 14:27:10 +0530

    Update title from 'Frontend Developer' to 'Full Stack Developer'

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,7 +12,7 @@ const Home = () => {
       <div className=" max-w-screen-lg mx-auto flex flex-col md:flex-row items-center justify-center h-full px-4">
         <div className="flex flex-col justify-center h-full">
           <h2 className="text-4xl sm:text-7xl font-bold text-white">
-            I'm a Frontend Developer
+            I'm a Full Stack Developer
           </h2>
           <p className="text-gray-500 py-4 max-w-md">
                 I am passionate about crafting dynamic, user-friendly applications that deliver seamless 
